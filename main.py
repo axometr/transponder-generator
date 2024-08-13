f5314976df5fa3292ce84e2b1915f167b3883cdb
Axometr <122098015+axometr@users.noreply.github.com>
2024-08-13 17:31:33 +0100

    more work

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import random
+
 # squawk codes can only have digits 0-7 in them
 digits = "01234567"
+transCode = ""
+# index the generator codes as to not regenerate
+generatedCodes = []
+# ICAO emergency codes
+forbiddenCodes = [7500, 7600, 7700]
+
+
+for i in range(0,4):
+    randDigit = random.choice(digits)
+    transCode += randDigit
